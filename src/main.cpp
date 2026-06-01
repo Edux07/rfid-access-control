42b0730cabf867a2becd7e83e9a0fae6febf8335
Edux07 <hernandezedu22@gmail.com>
2026-06-01 20:57:04 +0200

    feature(Servo Add): add the Servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+int servoPin = 8;
+Servo miservo;
+
+void servoAttach(Servo &servo);
+void moveServo(Servo &servo, int degrees);
+void servoSequence(Servo &servo);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  servoAttach(miservo);
+  servoSequence(miservo);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void servoAttach(Servo &servo) {
+  servo.attach(servoPin);
+}
+
+void moveServo(Servo &servo, int degrees) {
+  servo.write(degrees);
+}
+
+void servoSequence(Servo &servo){
+  moveServo(miservo, 90);
+  delay(2000);
+  
+  moveServo(miservo, 0);
+  delay(2000);
+
+  moveServo(miservo, 90);
+  delay(2000);
 }
